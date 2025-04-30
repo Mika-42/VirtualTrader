3a9389e1d32c3b800173442c71bd8392b524aeac
Matis <matis.mikael@gmail.com>
2025-04-30 10:04:05 +0200

    + merge menu.html with menu.php

--- menu.php
+++ menu.php
@@ -31,28 +31,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="loginPage.css">
-  <title>Menu</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="loginPage.css">
+    <title>Menu</title>
 </head>
 <body>
 
 <aside id="login-view">
-  <h1 id="welcome">Hi Foo !</h1> <!-- todo insert username -->
+    <h1 id="welcome">Hi Foo !</h1> <!-- todo insert username -->
 
-  <form id="menu-form" method="post">
+    <form id="menu-form" method="post">
 
-    <fieldset class="menu-field">
-      <input id="continue" class="menu-btn" type="submit" value="CONTINUE">
-      <input id="reset" class="menu-btn" type="submit" value="RESET">
-    </fieldset>
+        <fieldset class="menu-field">
+            <input id="continue" class="menu-btn" type="submit" value="CONTINUE">
+            <input id="reset" class="menu-btn" type="submit" value="RESET">
+        </fieldset>
 
-    <fieldset class="menu-field">
-      <input id="log-out" type="submit" value="LOGOUT">
-    </fieldset>
-  </form>
+        <fieldset class="menu-field">
+            <input id="log-out" type="submit" value="LOGOUT">
+        </fieldset>
+    </form>
 </aside>
-
 
 </body>
 </html>
