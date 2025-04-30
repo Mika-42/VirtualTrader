4ffde8540050c952363bca80c3db681e92b28621
unnebanane <ENZOPAPE2006@gmail.com>
2025-04-30 19:36:16 +0200

    nouvelle fonction pour choper l'id et mise en // d'une fonction inutile mais a garder pour usage ulterieur

--- db_connexion.php
+++ db_connexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$databaseName = "VirtualTrader";
+$databaseName = "virtualproject";
 
 //on se connecte a la base de donnée
 $connect = mysqli_connect($servername, $username, $password, $databaseName);
@@ -17,13 +17,18 @@ if (isset($_SESSION['game_status']) && $_SESSION['game_status'] == 'lost') {
     exit();
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action_type'])) {
-    $user_id = $_SESSION['user_id'];
-    $action_type = $_POST['action_type'];
-    $quantity = $_POST['quantity'];
-
-    $query = "INSERT INTO action (user_id, action_type, quantity) VALUES ($user_id, $action_type, $quantity )";
-    mysqli_query($connect, $query);
+function getCurrentUser_Id() {
+ return $_GET['id']; // Retourner l'ID de l'utilisateur
 }
+
+//if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action_type'])) {
+//    $user_id = $_SESSION['user_id'];
+//   $action_type = $_POST['action_type'];
+//   $quantity = $_POST['quantity'];
+//
+//   $query = "INSERT INTO action (user_id, action_type, quantity) VALUES ($user_id, $action_type, $quantity )";
+//    mysqli_query($connect, $query);
+//}
+
 ?>
 
